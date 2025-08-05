e3efe59cc94dd468bfc41ae43340437af3dfc155
alexfurst1 <143307271+alexfurst1@users.noreply.github.com>
2025-08-04 22:08:42 -0400

    Stuck on longest common prefix, will revisit. Finished merge_two_sorted_lists but don't fully understand the solution

--- leetcode/merge_two_sorted_lists.py
+++ leetcode/merge_two_sorted_lists.py
@@ -1,25 +1,23 @@
 # Definition for singly-linked list.
-from typing import Optional # imported this because it was recommended
-
-
 class ListNode:
      def __init__(self, val=0, next=None):
          self.val = val
          self.next = next
 
-def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    if list1 or list2:
-        if list1.val <= list2.val:
-            temp = list1.next
-            list1.next = list2
-            list1.next.next = temp
-            return self.mergeTwoLists(self.list1,self.list2.next)
-        else:
-            temp = list1
-            list1 = list2
-            list1.next = temp
-            return self.mergeTwoLists(self.list1,self.list2.next)
-        
-    return list1
-    
+from typing import Optional
 
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:      # list1 empty → nothing left to merge there
+            return list2
+        if list2 is None:      # list2 empty → splice the rest of list1
+            return list1
+
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next,list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1,list2.next)
+            return list2
+        
